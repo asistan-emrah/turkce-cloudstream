8f8c24b7d725dce39fb5f77b1247dc86698b0ce5
asistan-emrah <asistanemrah@gmail.com>
2024-12-19 19:57:44 +0300

    Create build.gradle.kts

---
+++ FilmEkseniProvider/build.gradle.kts
@@ -0,0 +1,26 @@
+// use an integer for version numbers
+version = 1
+
+
+android {
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    defaultConfig {
+        manifestPlaceholders["pluginClassName"] = "com.filmekseni.FilmEkseniProvider"
+    }
+}
+
+dependencies {
+    implementation("com.lagradost:cloudstream3:pre-release")
+}
+

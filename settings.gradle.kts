f2c452833827e12a088e9d1235b0624e98c3c673
asistan-emrah <asistanemrah@gmail.com>
2024-12-21 11:04:17 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,18 @@
 rootProject.name = "turkce-cloudstream"
 
-// Alt projeleri ekle
-include(":FilmEkseni")
-include(":DiziDay")
+// Include all directories with build.gradle.kts
+rootDir.walk()
+    .maxDepth(1)
+    .filter { it.isDirectory && it != rootDir && File(it, "build.gradle.kts").exists() }
+    .forEach { 
+        include(":${it.name}")
+    }
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+}

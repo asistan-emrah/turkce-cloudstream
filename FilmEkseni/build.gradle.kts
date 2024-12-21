32e02ec9d25ba0455782ee5162beb35849669da1
asistan-emrah <asistanemrah@gmail.com>
2024-12-21 12:45:01 +0300

    Update build.gradle.kts

--- FilmEkseni/build.gradle.kts
+++ FilmEkseni/build.gradle.kts
@@ -1,38 +1,18 @@
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
+version = 3
 
-android {
-    compileSdk = 33
+cloudstream {
+    authors     = listOf("asistan-emrah")
+    language    = "tr"
+    description = "Film modun geldiyse yüksek kalitede en yeni filmleri izle, 1080p izleyebileceğiniz reklamsiz film sitesi."
 
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 33
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-dependencies {
-    implementation("com.lagradost:cloudstream3:pre-release")
-}
-
-tasks.register("make") {
-    doLast {
-        val buildDir = file("$rootDir/build/plugins")
-        buildDir.mkdirs()
-        
-        File(buildDir, "FilmEkseni.cs3").writeBytes(
-            File(layout.buildDirectory.get().asFile, "outputs/aar/${project.name}-debug.aar")
-                .readBytes()
-        )
-    }
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Movie")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.filmmodu17.com&sz=%size%"
 }

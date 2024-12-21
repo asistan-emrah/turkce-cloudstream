6106125e1ea6c94a83fd14f8e67e36c4a674e2d8
asistan-emrah <asistanemrah@gmail.com>
2024-12-21 10:53:06 +0300

    Create build.gradle.kts

---
+++ FilmEkseni/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("com.lagradost:cloudstream3:pre-release")
+}
+
+// CloudStream yapılandırması
+android.applicationVariants.all {
+    outputs.all {
+        val outputFileName = "FilmEkseni.cs3"
+        file("$rootDir/build/plugins").mkdirs()
+        file("$rootDir/build/plugins/$outputFileName").delete()
+        outputFile.renameTo(file("$rootDir/build/plugins/$outputFileName"))
+    }
+}

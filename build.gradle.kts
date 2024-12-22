6ff11fa1677a2f3010f76255ac1c8409fa7372ac
asistan-emrah <asistanemrah@gmail.com>
2024-12-22 13:08:24 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,4 @@
-
-
-// Top-level build.gradle.kts
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
@@ -16,14 +8,6 @@ buildscript {
 plugins {
     id("com.android.library") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
 }
 
 tasks.register("clean", Delete::class) {

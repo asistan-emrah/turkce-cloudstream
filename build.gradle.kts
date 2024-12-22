33a75192a923707aae0bb6433a342790563f5ce3
asistan-emrah <asistanemrah@gmail.com>
2024-12-22 12:15:39 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,22 @@
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
     }
+}
+
+plugins {
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
 
 tasks.register("make") {

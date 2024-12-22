9d4da0930c0a98cfebf99c8f578907a44866daa9
asistan-emrah <asistanemrah@gmail.com>
2024-12-22 12:29:13 +0300

    Update build.gradle.kts

--- FilmEkseni/build.gradle.kts
+++ FilmEkseni/build.gradle.kts
@@ -1,33 +1,39 @@
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
+// use an integer for version numbers
+version = 1
 
 android {
-    compileSdk = 33
-
+    compileSdkVersion(33)
     defaultConfig {
-        minSdk = 21
-        targetSdk = 33
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
+        minSdkVersion(21)
+        targetSdkVersion(33)
     }
 }
 
 dependencies {
-    implementation("com.github.recloudstream:cloudstream:pre-release")
+    implementation("com.lagradost:cloudstream3:pre-release")
 }
 
-tasks.register<Jar>("makeDebugJar") {
-    dependsOn("assembleDebug")
-    from("$buildDir/intermediates/aar_main_jar/debug/classes.jar")
-    archiveFileName.set("FilmEkseni.cs3")
-    destinationDirectory.set(File(rootProject.buildDir, "plugins"))
+cloudstream {
+    language = "tr"
+    // All of these properties are optional, you can safely remove them
+
+    description = "FilmEkseni için CloudStream eklentisi"
+    authors = listOf("asistan-emrah")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 1 if unspecified
+
+    // List of video source types. Users can filter for extensions in a given category.
+    // You can find a list of avaliable types here: https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime"
+    )
 }

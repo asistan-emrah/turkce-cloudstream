5b175ca67849f343511757cb6d3b1d359405029e
asistan-emrah <asistanemrah@gmail.com>
2024-12-25 22:31:57 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,20 @@
+rootProject.name = "turkce-cloudstream"
+include(":FilmEkseni", ":DiziDay")
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
         google()
         mavenCentral()
         maven("https://jitpack.io")
     }
 }
 
-
-rootProject.name = "turkce-cloudstream"
-include(":FilmEkseni", ":DiziDay")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}

57d4d0e758b1369cf849ca16f82aa55419a33a1e
asistan-emrah <asistanemrah@gmail.com>
2024-12-27 19:35:09 +0300

    Update build.gradle.kts

--- FilmModu/build.gradle.kts
+++ FilmModu/build.gradle.kts
@@ -1,14 +1,13 @@
 // use an integer for version numbers
 version = 1
 
-
 android {
     defaultConfig {
-        // Sürüm adını ve kodunu buradan ayarlayabilirsiniz
         versionName = "1.0.0"
         versionCode = 1
     }
 }
 
 dependencies {
+    implementation("org.jsoup:jsoup:1.15.3")
 }
